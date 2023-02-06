072649b2ce3670432edf27d8e54082070f99201b
ArunKumar <69241947+ArunKumar-webdev@users.noreply.github.com>
2023-02-06 12:24:53 +0530

    Update Levelpane.tsx

--- components/Levelpane.tsx
+++ components/Levelpane.tsx
@@ -3,12 +3,24 @@ import { useState } from 'react';
 
 let array: any = [
     {
-        "level": 1
+        "level": 1,
+        "child": [
+            "1.1",
+            "1.2",
+            "1.3",
+            "1.4"
+        ],
+        "innerchild": []
     },
     {
-        "level": 2
+        "level": 2,
+        "child": [
+            "2.1",
+            "2.2"
+        ],
+        "innerchild": []
     }
-];
+]
 
 export default function Levelpane() {
 
@@ -44,7 +56,7 @@ export default function Levelpane() {
                 return element.level == innerChild.split('.')[0];
             });
 
-            if (check && check[0].innerchild[innerChild] && Object.keys(check[0].innerchild).length) {
+            if (check?.length > 0 && check[0].innerchild[innerChild] && Object.keys(check[0].innerchild).length) {
                 // levels[Number(innerChild.split('.')[0]) - 1]['innerchild'][innerChild].count += 1;
                 for (let t = 0; t < levels.length; t++) {
                     if (levels[t].level == innerChild.split('.')[0]) {
@@ -84,10 +96,20 @@ export default function Levelpane() {
             alert('Tree List Should Contain More Than One Data To Perform Delete ')
         }
     }
+    function prev() {
+        setSelectedLi(selectedLi - 1);
+        setinnerChild(((Number(innerChild) - 0.1).toFixed(1)).toString());
+    }
+    function next() {
+        debugger
+        setSelectedLi(selectedLi + 1);
+        setinnerChild(((Number(innerChild) + 0.1).toFixed(1)).toString());
+    }
 
     const handleClickColorChange = (e: any, index: any) => {
-        console.log(e.target.childNodes[1].data)
-        setinnerChild(e.target.childNodes[1].data)
+        debugger
+        console.log(e.target.childNodes[e.target.childNodes.length - 1].wholeText)
+        setinnerChild(e.target.childNodes[e.target.childNodes.length - 1].wholeText)
         setSelectedLi(index);
     };
 
@@ -105,12 +127,12 @@ export default function Levelpane() {
                                 style={{ backgroundColor: index === selectedLi ? 'white' : 'transparent' }}
                                 onClick={(e: any) => handleClickColorChange(e, index)} >Level {value.level}</div>
 
-                            {value.child?.map((childvalue: any, index: number) => {
+                            {value.child?.map((childvalue: any, indexchild: number) => {
                                 return (
                                     <>
-                                        <div key={index + 100} className='font-semibold text-[#015389ff] indent-5'
-                                            style={{ backgroundColor: index + (100 * value.level) === selectedLi ? 'white' : 'transparent' }}
-                                            onClick={(e: any) => handleClickColorChange(e, index + (100 * value.level))}>
+                                        <div key={indexchild + 100} className='font-semibold text-[#015389ff] indent-5'
+                                            style={{ backgroundColor: indexchild + (100 * value.level) === selectedLi ? 'white' : 'transparent' }}
+                                            onClick={(e: any) => handleClickColorChange(e, indexchild + (100 * value.level))}>
                                             Level {childvalue}
 
                                         </div>
@@ -142,10 +164,10 @@ export default function Levelpane() {
                         </button>
                     </div>
                     <div id='rightbt'>
-                        <button className='w-8 h-8 font-bold bg-[#015389ff] text-white '>
+                        <button className='w-8 h-8 font-bold bg-[#015389ff] text-white' onClick={prev}>
                             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className='m-auto' width="12" height="12" x="0" y="0" viewBox="0 0 512 512"><g><path d="M490.667 45.729C490.667 20.521 470.01 0 444.615 0c-6.896 0-13.625 1.563-19.99 4.646l-.031.021c-69.958 34.104-169.188 91.271-256.729 141.708-45.729 26.354-88.917 51.229-121.74 69.229-15.521 8.5-24.792 23.604-24.792 40.396s9.271 31.896 24.792 40.396c32.896 18.021 76.188 42.958 122.021 69.375 87.448 50.375 186.563 107.479 256.438 141.542 6.365 3.104 13.104 4.688 20.031 4.688 25.396 0 46.052-20.521 46.052-45.75l-.021-210.271.021-210.251z" fill="#ffffff" data-original="#000000"></path></g></svg>
                         </button>
-                        <button className='w-8 h-8 font-bold bg-[#015389ff] text-white ml-3'>
+                        <button className='w-8 h-8 font-bold bg-[#015389ff] text-white ml-3' onClick={next}>
                             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" className='m-auto' width="18" height="18" x="0" y="0" viewBox="0 0 32 32" ><g><path fill="#ffffff" d="m22.712 15.287-10-10a1.05 1.05 0 0 0-1.1-.212A1 1 0 0 0 11 6v20a1 1 0 0 0 .612.925 1.038 1.038 0 0 0 1.1-.213l10-10a1.012 1.012 0 0 0 0-1.425z" data-original="#000000"></path></g></svg></button>
                     </div>
                 </div>
